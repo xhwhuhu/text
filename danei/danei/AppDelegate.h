6328be83c49f0699e63514cff3c8226195830957
xhwhuhu <xhwhuhu@AppledeMacBook-Pro.local>
2016-06-08 16:01:16 +0800

    新建项目

---
+++ danei/danei/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  danei
+//
+//  Created by xhwhuhu on 16/6/8.
+//  Copyright © 2016年 xhw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ danei/danei/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  danei
+//
+//  Created by xhwhuhu on 16/6/8.
+//  Copyright © 2016年 xhw. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
